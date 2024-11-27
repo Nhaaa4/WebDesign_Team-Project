d2f05d754c159e7d2d8d17108a4ab9767cdfc15d
SeilaCP <manhseila@gmail.com>
2024-11-27 15:00:01 +0700

    login and signup

---
+++ login/script.js
@@ -0,0 +1,18 @@
+const userEmail = document.getElementById('email');
+const userPassword = document.getElementById('password');
+const login = document.getElementById('login');
+
+function log()
+{
+    if ((userEmail.value == false) && (userPassword.value == false))
+    {
+       userEmail.style.border = 'solid red';
+       userPassword.style.border = 'solid red';
+    }else
+    {
+        userEmail.style.border = 'none';
+       userPassword.style.border = 'none';
+        console.log(userEmail.value);
+        console.log(userPassword.value);
+    }
+}
